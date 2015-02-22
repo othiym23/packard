10428149f2dce2d869c02dfeaab2c61c7cef97de
Forrest L Norvell <forrest@npmjs.com>
2015-02-22 00:48:27 -0800

    `packard artists` works

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,24 @@
+const scanArtists = require("./artists.js")
+
 const yargs = require("yargs")
                 .usage("Usage: $0 [options] <command>")
+                .help("h")
+                .alias("h", "help")
+                .command("artists", "generate a list of artists from roots")
                 .array("R")
                 .alias("R", "root")
+                .required("R", "must have at least one tree to scan")
                 .describe("R", "directory root for an Artist/Album tree")
+                .version(() => require("../package").version)
                 .demand(1)
 
 switch (yargs.argv._[0]) {
   case "artists":
-    console.log("artists go here")
+    scanArtists(yargs.argv.R).then(sorted => {
+      for (let a of sorted) {
+        console.log("%s [%s]", a.name, a.getSize(1024 * 1024))
+      }
+    }).catch(error => console.error("HURF DURF", error.stack))
     break
   default:
     yargs.showHelp()
--- test/cli-basic.js
+++ test/cli-basic.js
@@ -1,7 +1,9 @@
 var resolve = require("path").resolve
 var relative = require("path").relative
 
+var mkdirp = require("mkdirp")
 var nixt = require("nixt")
+var rimraf = require("rimraf")
 var test = require("tap").test
 
 var p = resolve(__dirname, "../lib/cli.js")
@@ -9,19 +11,40 @@ var r = relative(process.cwd(), p)
 
 var lines = function () {/*
 
+Commands:
+  artists    generate a list of artists from roots
+
 Options:
-  -R, --root  directory root for an Artist/Album tree
+  -h, --help  Show help
+  -R, --root  directory root for an Artist/Album tree  [required]
+  --version   Show version number
 
 Not enough non-option arguments: got 0, need at least 1
 */}.toString().split("\n").slice(1, -1)
 
 var prolog = "Usage: node "+r+" [options] <command>"
 
+var root = resolve(__dirname, "blank-tree")
+
+test("setup", function (t) {
+  rimraf.sync(root)
+  mkdirp(root, function (error) {
+    t.ifError(error, "made blank media directory")
+    t.end()
+  })
+})
+
 test("packard", function (t) {
   var expected = [prolog].concat(lines).join("\n")
   nixt()
     .run("node "+p)
-    .stderr(expected)
+    .expect(function (r) {
+      var trimmed = r.stderr
+                     .split("\n")
+                     .map(function (l) { return l.replace(/\s+$/, "") })
+                     .join("\n")
+      t.equal(trimmed, expected, "got expected default output")
+    })
     .code(1)
     .end(function (e) {
       t.ifError(e, "got expected default output")
@@ -30,11 +53,32 @@ test("packard", function (t) {
 })
 
 test("packard unknown", function (t) {
-  var unknown = [prolog].concat(lines.slice(0, -1)).join("\n")
+  var trailer = function () {/*
+Missing required arguments: R
+must have at least one tree to scan
+  */}.toString().split("\n").slice(1, -1)
+  var unknown = [prolog].concat(lines.slice(0, -1), trailer).join("\n")
   nixt()
     .run("node "+p+" unknown")
-    .stderr(unknown)
+    .expect(function (r) {
+      var trimmed = r.stderr
+                     .split("\n")
+                     .map(function (l) { return l.replace(/\s+$/, "") })
+                     .join("\n")
+      t.equal(trimmed, unknown, "got expected default output")
+    })
     .code(1)
+    .end(function (e) {
+      t.ifError(e, "got expected default output")
+      t.end()
+    })
+})
+
+test("packard --version", function (t) {
+  nixt()
+    .run("node "+p+" --version")
+    .stdout(require("../package.json").version)
+    .code(0)
     .end(function (e) {
       t.ifError(e, "got expected default output")
       t.end()
@@ -43,10 +87,19 @@ test("packard unknown", function (t) {
 
 test("packard artists", function (t) {
   nixt()
-    .run("node "+p+" artists")
-    .stdout("artists go here")
+    .run("node "+p+" artists -R " + root)
+    .stdout("")
+    .stderr("")
+    .code(0)
     .end(function (e) {
       t.ifError(e, "got expected default output")
       t.end()
     })
+})
+
+test("cleanup", function (t) {
+  rimraf(root, function (error) {
+    t.ifError(error, "made blank media directory")
+    t.end()
+  })
 })
