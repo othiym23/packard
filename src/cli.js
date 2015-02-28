f825fb960a7ca543555626ddf4a1dea716da2a57
Forrest L Norvell <forrest@npmjs.com>
2015-02-27 23:07:56 -0800

    wired up config file

--- src/cli.js
+++ src/cli.js
@@ -1,25 +1,43 @@
 #!/usr/bin/env node
-const scanArtists = require("./artists.js")
+const scanArtists = require('./artists.js')
+const untildify = require('untildify')
 
-const yargs = require("yargs")
-                .usage("Usage: $0 [options] <command>")
-                .help("h")
-                .alias("h", "help")
-                .command("artists", "generate a list of artists from roots")
-                .array("R")
-                .alias("R", "root")
-                .required("R", "must have at least one tree to scan")
-                .describe("R", "directory root for an Artist/Album tree")
-                .version(() => require("../package").version)
+const config = require('rc')(
+  'packard',
+  {
+    roots: []
+  },
+  [] // don't want rc interpreting argv
+)
+
+const yargs = require('yargs')
+                .usage('Usage: $0 [options] <command>')
+                .help('h')
+                .alias('h', 'help')
+                .command('artists', 'generate a list of artists from roots')
+                .version(() => require('../package').version)
                 .demand(1)
 
+let options = {
+  R: {
+    alias: 'root',
+    array: true,
+    describe: 'directory root for an Artist/Album tree',
+    required: 'must have at least one tree to scan',
+    default: config.roots
+  }
+}
+
 switch (yargs.argv._[0]) {
-  case "artists":
-    scanArtists(yargs.argv.R).then(sorted => {
+  case 'artists':
+    let argv = yargs.reset()
+         .options(options)
+         .argv
+    scanArtists(argv.R.map(r => untildify(r))).then(sorted => {
       for (let a of sorted) {
-        console.log("%s [%s]", a.name, a.getSize(1024 * 1024))
+        console.log('%s [%s]', a.name, a.getSize(1024 * 1024))
       }
-    }).catch(error => console.error("HURF DURF", error.stack))
+    }).catch(error => console.error('HURF DURF', error.stack))
     break
   default:
     yargs.showHelp()
--- test/cli-basic.js
+++ test/cli-basic.js
@@ -1,12 +1,12 @@
-var resolve = require("path").resolve
-var relative = require("path").relative
+var resolve = require('path').resolve
+var relative = require('path').relative
 
-var mkdirp = require("mkdirp")
-var nixt = require("nixt")
-var rimraf = require("rimraf")
-var test = require("tap").test
+var mkdirp = require('mkdirp')
+var nixt = require('nixt')
+var rimraf = require('rimraf')
+var test = require('tap').test
 
-var p = resolve(__dirname, "../lib/cli.js")
+var p = resolve(__dirname, '../lib/cli.js')
 var r = relative(process.cwd(), p)
 
 var lines = function () {/*
@@ -16,90 +16,85 @@ Commands:
 
 Options:
   -h, --help  Show help
-  -R, --root  directory root for an Artist/Album tree  [required]
   --version   Show version number
 
 Not enough non-option arguments: got 0, need at least 1
-*/}.toString().split("\n").slice(1, -1)
+*/}.toString().split('\n').slice(1, -1)
 
-var prolog = "Usage: node "+r+" [options] <command>"
+var prolog = 'Usage: node ' + r + ' [options] <command>'
 
-var root = resolve(__dirname, "blank-tree")
+var root = resolve(__dirname, 'blank-tree')
 
-test("setup", function (t) {
+test('setup', function (t) {
   rimraf.sync(root)
   mkdirp(root, function (error) {
-    t.ifError(error, "made blank media directory")
+    t.ifError(error, 'made blank media directory')
     t.end()
   })
 })
 
-test("packard", function (t) {
-  var expected = [prolog].concat(lines).join("\n")
+test('packard', function (t) {
+  var expected = [prolog].concat(lines).join('\n')
   nixt()
-    .run("node "+p)
+    .run('node ' + p)
     .expect(function (r) {
       var trimmed = r.stderr
-                     .split("\n")
-                     .map(function (l) { return l.replace(/\s+$/, "") })
-                     .join("\n")
-      t.equal(trimmed, expected, "got expected default output")
+                     .split('\n')
+                     .map(function (l) { return l.replace(/\s+$/, '') })
+                     .join('\n')
+      t.equal(trimmed, expected, '"packard" is missing a required parameter')
     })
     .code(1)
     .end(function (e) {
-      t.ifError(e, "got expected default output")
+      t.ifError(e, 'no error on exit')
       t.end()
     })
 })
 
-test("packard unknown", function (t) {
-  var trailer = function () {/*
-Missing required arguments: R
-must have at least one tree to scan
-  */}.toString().split("\n").slice(1, -1)
-  var unknown = [prolog].concat(lines.slice(0, -1), trailer).join("\n")
+test('packard unknown', function (t) {
+  var unknown = [prolog].concat(lines.slice(0, -1)).join('\n')
   nixt()
-    .run("node "+p+" unknown")
+    .run('node ' + p + ' unknown')
     .expect(function (r) {
       var trimmed = r.stderr
-                     .split("\n")
-                     .map(function (l) { return l.replace(/\s+$/, "") })
-                     .join("\n")
-      t.equal(trimmed, unknown, "got expected default output")
+                     .split('\n')
+                     .map(function (l) { return l.replace(/\s+$/, '') })
+                     .join('\n')
+      t.equal(trimmed, unknown, '"packard unknown" doesn\t have any missing parameters')
     })
     .code(1)
     .end(function (e) {
-      t.ifError(e, "got expected default output")
+      t.ifError(e, 'got expected default output')
       t.end()
     })
 })
 
-test("packard --version", function (t) {
+test('packard --version', function (t) {
   nixt()
-    .run("node "+p+" --version")
-    .stdout(require("../package.json").version)
+    .run('node ' + p + ' --version')
+    .stdout(require('../package.json').version)
     .code(0)
     .end(function (e) {
-      t.ifError(e, "got expected default output")
+      t.ifError(e, 'got expected default output')
       t.end()
     })
 })
 
-test("packard artists", function (t) {
+test('packard artists', function (t) {
   nixt()
-    .run("node "+p+" artists -R " + root)
-    .stdout("")
-    .stderr("")
+    .run('node ' + p + ' artists -R ' + root)
+    .stdout('')
+    .stderr('')
     .code(0)
     .end(function (e) {
-      t.ifError(e, "got expected default output")
+      t.ifError(e, 'got expected default output')
       t.end()
     })
 })
 
-test("cleanup", function (t) {
+test('cleanup', function (t) {
   rimraf(root, function (error) {
-    t.ifError(error, "made blank media directory")
+    t.ifError(error, 'made blank media directory')
     t.end()
   })
 })
