b52d5be618b66a47ae1168c0d8b60f9e85de9d57
Forrest L Norvell <forrest@npmjs.com>
2015-09-07 00:14:12 -0700

    metadata: handle AVI files

--- src/metadata/index.js
+++ src/metadata/index.js
@@ -8,6 +8,7 @@ import Promise from 'bluebird'
 import scanFLAC from '../flac/scan.js'
 import { unpack as unzip } from '../utils/zip.js'
 import Cover from '../models/cover.js'
+import File from '../models/cover.js'
 
 const stat = promisify(fs.stat)
 
@@ -22,9 +23,11 @@ function scan (unpackedFiles, trackers) {
         case '.pdf':
         case '.png':
           return stat(path).then(stats => new Cover(path, stats))
+        case '.avi':
+          return stat(path).then(stats => new File(path, stats))
         default:
           log.error('scan', "don't recognize type of", path)
-          throw new Error("don't recognize type of " + path)
+          return new Error("don't recognize type of " + path)
       }
     }
   )
