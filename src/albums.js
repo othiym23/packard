1be9878a44f4c222faefd929c1a8783bdae6a964
Forrest L Norvell <forrest@npmjs.com>
2015-09-21 01:45:20 -0700

    sort: use moment with explicit templates

--- src/albums.js
+++ src/albums.js
@@ -8,9 +8,19 @@ import flatten from './flatten-tracks.js'
 import readArtists from './read-fs-artists.js'
 import scanFLAC from './flac/scan.js'
 
+function toMoment (date) {
+  let components = (date || '1970-01-01').split('-')
+  switch (components.length) {
+    case 1: return moment(date, 'YYYY')
+    case 2: return moment(date, 'YYYY-MM')
+    case 3: return moment(date, 'YYYY-MM-DD')
+    default: throw new TypeError(date + 'is not a recognizable date')
+  }
+}
+
 function byDate (a, b) {
-  let am = moment(a.date)
-  let bm = moment(b.date)
+  let am = toMoment(a.date)
+  let bm = toMoment(b.date)
 
   if (am.isBefore(bm)) {
     return -1
