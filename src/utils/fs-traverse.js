d579c51d69610a5d38459ea22093e29f40fcc1c0
Forrest L Norvell <forrest@npmjs.com>
2015-11-27 20:02:35 -0800

    traverse: handle file as well as directory roots
    Turn the logic inside-out so that the test for root type happens at the
    beginning of the traversal. Also requires a pass to filter out the
    undefineds left behind by cruft being filtered from a directory scan.
    Also use a set for finding the cruft because why not.

--- src/utils/fs-traverse.js
+++ src/utils/fs-traverse.js
@@ -1,28 +1,29 @@
 import fs from 'graceful-fs'
-import { resolve } from 'path'
+import { basename, resolve } from 'path'
 
 import { promisify } from 'bluebird'
 
 const readdir = promisify(fs.readdir)
 const stat = promisify(fs.stat)
 
-const cruft = [
+const cruft = new Set([
   '.DS_Store',    // OS X metadata is very cluttery
   '.AppleDouble', // see above
   '.localized',   // OS X localization
   'Thumbs.db',    // yes, I do run Windows sometimes
   '.Parent'       // I have no idea
-]
+])
 
-export default function traverse (root, visit) {
-  return readdir(root)
-           .filter(e => cruft.indexOf(e) === -1)
-           .map(e => {
-             const path = resolve(root, e)
-             return stat(path).then(stats => {
-               if (stats.isDirectory()) return traverse(path, visit)
-               else if (stats.isFile()) return visit(path, stats)
-               else throw new Error(path + ' is an unsupported thing.')
-             })
-           })
+export default function traverse (path, visit) {
+  return stat(path).then(stats => {
+    if (stats.isDirectory()) {
+      return readdir(path)
+               .map(e => traverse(resolve(path, e), visit))
+               .filter(e => e)
+    } else if (stats.isFile()) {
+      if (!cruft.has(basename(path))) return visit(path, stats)
+    } else {
+      throw new Error(path + ' is an unsupported thing.')
+    }
+  })
 }
