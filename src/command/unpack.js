313384581836d6895caa66c0cadfa201b1fc5f6b
Forrest L Norvell <forrest@npmjs.com>
2015-11-29 14:33:21 -0800

    unpack: merge in metadata/index.js
    unpack is kind of unwieldy now, but metadata/index.js was an inelegant
    extraction.

--- src/command/unpack.js
+++ src/command/unpack.js
@@ -1,5 +1,5 @@
-import { writeFile as writeFileCB } from 'graceful-fs'
-import { join, resolve } from 'path'
+import { writeFile as writeFileCB, stat as statCB } from 'graceful-fs'
+import { dirname, extname, join, resolve } from 'path'
 import { randomBytes } from 'crypto'
 import { tmpdir } from 'os'
 
@@ -7,16 +7,19 @@ import globCB from 'glob'
 import log from 'npmlog'
 import rimrafCB from 'rimraf'
 import untildify from 'untildify'
-import { promisify } from 'bluebird'
 import Bluebird from 'bluebird'
+import { unpack as unzip } from '../utils/zip.js'
+import { Cover, File } from '@packard/model'
 
 import albumsFromFLACTracks from '../flac/albums-from-tracks.js'
 import makePlaylist from '../utils/make-playlist.js'
+import scanFLAC from '../flac/scan.js'
 import { place, moveToArchive } from '../mover.js'
-import { extractRelease } from '../metadata/index.js'
 
+import { promisify } from 'bluebird'
 const glob = promisify(globCB)
 const rimraf = promisify(rimrafCB)
+const stat = promisify(statCB)
 const writeFile = promisify(writeFileCB)
 
 const covers = new Map()
@@ -25,7 +28,9 @@ const tmp = join(tmpdir(), 'packard-' + randomBytes(8).toString('hex'))
 export default function unpack (target, staging, archiveRoot, playlist) {
   log.enableProgress()
   const groups = new Map()
+
   let locate = Bluebird.resolve(target.files)
+
   if (target.roots && target.roots[0] && target.pattern) {
     locate = locate.then(files => {
       log.verbose('unpack', 'initial files', files)
@@ -49,17 +54,22 @@ export default function unpack (target, staging, archiveRoot, playlist) {
 
     log.verbose('unpack', 'processing', files)
     files.forEach(f => groups.set(f, log.newGroup('process: ' + f)))
-    return Bluebird.map(
-      files,
-      f => extractRelease(f, tmp, covers, groups),
-      {concurrency: 2}
-    )
-  }).then(m => {
+    return files
+  }).map(
+    f => extractRelease(f, tmp, covers, groups),
+    {concurrency: 2}
+  ).then(m => {
     return place(albumsFromFLACTracks(m, covers), staging, groups)
-  }).then(placed => {
-    if (!archiveRoot) return Bluebird.resolve(placed)
-    return moveToArchive(placed, archiveRoot, groups).then(() => placed)
-  }).then(albums => {
+  })
+
+  if (archiveRoot) {
+    locate = locate.then(
+      placed => moveToArchive(placed, archiveRoot, groups)
+                  .then(() => placed)
+    )
+  }
+
+  locate = locate.then(albums => {
     log.disableProgress()
     report(albums, staging)
     if (archiveRoot) reportArchived(albums)
@@ -99,4 +109,54 @@ function reportArchived (albums) {
   for (let album of archived) {
     console.log(album.sourceArchive.path, '\n  ->', album.destArchive.path)
   }
+}
+
+function scan (unpackedFiles, trackers) {
+  return Bluebird.map(
+    unpackedFiles,
+    ({ path, sourceArchive, flacTrack }) => {
+      switch (extname(path)) {
+        case '.flac':
+          if (flacTrack) {
+            if (sourceArchive) flacTrack.sourceArchive = sourceArchive
+            return Bluebird.resolve(flacTrack)
+          } else {
+            return scanFLAC(path, trackers, { sourceArchive })
+          }
+          break
+        case '.jpg':
+        case '.pdf':
+        case '.png':
+          return stat(path).then(stats => new Cover(path, stats))
+        default:
+          log.warn('scan', "don't recognize type of", path)
+          return stat(path).then(stats => new File(path, stats))
+      }
+    }
+  )
+}
+
+function populateImages (list, covers) {
+  list.filter(e => e instanceof Cover)
+      .forEach(c => {
+        const directory = dirname(c.path)
+        if (!covers.get(directory)) {
+          log.silly('scan', 'creating image list for', directory)
+          covers.set(directory, [])
+        }
+        log.silly('scan', 'cover', c)
+        covers.get(directory).push(c)
+      })
+
+  return list
+}
+
+function extractRelease (zipfile, tmpdir, covers, trackers) {
+  log.verbose('extractReleaseMetadata', 'archive:', zipfile)
+
+  return unzip(zipfile, trackers, tmpdir)
+          .then(list => scan(list, trackers))
+          .then(list => populateImages(list, covers))
+          .then(list => list.filter(e => !(e instanceof Cover ||
+                                           e instanceof Error)))
 }
