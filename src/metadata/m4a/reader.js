8033d161348a8c81b977c86379dfb4f21c784f5d
Forrest L Norvell <forrest@npmjs.com>
2015-11-29 22:22:56 -0800

    metadata: split out M4A tag mappings as well

--- src/metadata/m4a/reader.js
+++ src/metadata/m4a/reader.js
@@ -6,61 +6,8 @@ import log from 'npmlog'
 import MP4Parser from 'mp4-parser'
 
 import { Album, Artist, AudioFile, Track } from '@packard/model'
+import { typeToStreamData, typeToTag, typeToMB } from './tag-maps.js'
 import { parseGapless, parseNormalization } from '../../utils/itunes.js'
-
-const atomToStreamData = new Map([
-  ['duration', 'duration']
-])
-
-const atomToTag = new Map([
-  ['©alb', 'album'],
-  ['©ART', 'artist'],
-  ['©cmt', 'comment'],
-  ['©day', 'date'],
-  ['©gen', 'genre'],
-  ['©nam', 'title'],
-  ['©too', 'encodedWith'],
-  ['aART', 'albumArtist'],
-  ['ASIN', 'ASIN'],
-  ['BARCODE', 'upc'],
-  ['covr', 'picture'],
-  ['cpil', 'isCompilation'],
-  ['disk', 'discs'],
-  ['pgap', 'hasPregap'],
-  ['purd', 'purchaseDate'],
-  ['soaa', 'sortAlbumArtist'],
-  ['soar', 'sortArtist'],
-  ['tmpo', 'bpm'],
-  ['trkn', 'tracks'],
-  ['iTunes_CDDB_IDs', 'cddb'],
-  ['iTunNORM', 'iTunesNormalization'],
-  ['iTunSMPB', 'iTunesGaplessData']
-])
-
-const atomToMB = new Map([
-  ['MusicBrainz Album Artist Id', 'albumArtistID'],
-  ['MusicBrainz Album Id', 'albumID'],
-  ['MusicBrainz Album Release Country', 'releaseCountry'],
-  ['MusicBrainz Album Status', 'status'],
-  ['MusicBrainz Album Type', 'type'],
-  ['MusicBrainz Artist Id', 'artistID'],
-  ['MusicBrainz Release Group Id', 'releaseGroupID'],
-  ['MusicBrainz Release Track Id', 'releaseTrackID'],
-  ['MusicBrainz Track Id', 'trackID'],
-  ['MusicIP PUID', 'puid'],
-  ['Acoustid Id', 'acoustID'],
-  ['ARTISTS', 'artists'],
-  ['CATALOGNUMBER', 'catalogID'],
-  ['DJMIXER', 'mixedBy'],
-  ['LABEL', 'label'],
-  ['LANGUAGE', 'language'],
-  ['MEDIA', 'media'],
-  ['SCRIPT', 'script']
-])
-
-// mp4-parser doesn't deal well with Apple's old iTunes atom types
-// (this is the UTF-8 out-of-universe character)
-const whoops = new Buffer([0xef, 0xbf, 0xbd])
 
 export default function reader (path, progressGroups, extras, onFinish, onError) {
   const name = basename(path)
@@ -77,16 +24,14 @@ export default function reader (path, progressGroups, extras, onFinish, onError)
 
   return throughWatcher
     .pipe(new MP4Parser())
-    .on('data', ({ type, value }) => {
-      let raw = new Buffer(type)
-      if (raw.indexOf(whoops) === 0) type = '©' + raw.slice(3).toString()
-      type = type.replace('----:com.apple.iTunes:', '')
-      if (atomToTag.get(type)) {
-        tags[atomToTag.get(type)] = value
-      } else if (atomToMB.get(type)) {
-        musicbrainzTags[atomToMB.get(type)] = value
-      } else if (atomToStreamData.get(type)) {
-        streamData[atomToStreamData.get(type)] = value
+    .on('data', ({ busted, value }) => {
+      const type = fixupType(busted)
+      if (typeToTag.get(type)) {
+        tags[typeToTag.get(type)] = value
+      } else if (typeToMB.get(type)) {
+        musicbrainzTags[typeToMB.get(type)] = value
+      } else if (typeToStreamData.get(type)) {
+        streamData[typeToStreamData.get(type)] = value
       } else {
         log.silly('m4a.read', 'unknown type', type, 'value', value)
       }
@@ -126,4 +71,13 @@ export default function reader (path, progressGroups, extras, onFinish, onError)
       const track = new Track(tags.title, album, artist, extras)
       onFinish({ track })
     })
+}
+
+// mp4-parser doesn't deal well with Apple's old iTunes atom types
+// (this is the UTF-8 out-of-universe character)
+const whoops = new Buffer([0xef, 0xbf, 0xbd])
+function fixupType (maybeBroken) {
+  let raw = new Buffer(maybeBroken)
+  if (raw.indexOf(whoops) === 0) maybeBroken = '©' + raw.slice(3).toString()
+  return maybeBroken.replace('----:com.apple.iTunes:', '')
 }
---
+++ src/metadata/m4a/tag-maps.js
@@ -0,0 +1,62 @@
+export const typeToStreamData = new Map([
+  ['duration', 'duration'],
+  // ['bitsPerSample', 'bitsPerSample'],
+  // ['bytesToFirstFrame', 'bytesToFirstFrame'],
+  // ['channels', 'channels'],
+  // ['maxBlockSize', 'maxBlockSize'],
+  // ['maxFrameSize', 'maxFrameSize'],
+  // ['minBlockSize', 'minBlockSize'],
+  // ['minFrameSize', 'minFrameSize'],
+  // ['sampleRate', 'sampleRate'],
+  // ['samplesInStream', 'samplesInStream'],
+  ['TRAKTOR4', 'traktor4']
+])
+
+export const typeToTag = new Map([
+  ['©alb', 'album'],
+  ['©ART', 'artist'],
+  ['©cmt', 'comment'],
+  ['©day', 'date'],
+  ['©gen', 'genre'],
+  ['©nam', 'title'],
+  ['©too', 'encodedWith'],
+  ['aART', 'albumArtist'],
+  ['ASIN', 'ASIN'],
+  ['BARCODE', 'upc'],
+  ['covr', 'picture'],
+  ['cpil', 'isCompilation'],
+  ['disk', 'discs'], // split into disc/discs
+  ['pgap', 'hasPregap'],
+  ['purd', 'purchaseDate'],
+  ['soaa', 'sortAlbumArtist'],
+  ['soar', 'sortArtist'],
+  ['tmpo', 'bpm'],
+  ['trkn', 'tracks'], // split into index/tracks
+  ['iTunes_CDDB_IDs', 'cddb'],
+  ['iTunNORM', 'iTunesNormalization'],
+  ['iTunSMPB', 'iTunesGaplessData']
+  // ['INITIALKEY', 'initialKey']
+])
+
+export const typeToMB = new Map([
+  ['MusicBrainz Album Artist Id', 'albumArtistID'],
+  ['MusicBrainz Album Id', 'albumID'],
+  ['MusicBrainz Album Release Country', 'releaseCountry'],
+  ['MusicBrainz Album Status', 'status'],
+  ['MusicBrainz Album Type', 'type'],
+  ['MusicBrainz Artist Id', 'artistID'],
+  ['MusicBrainz Release Group Id', 'releaseGroupID'],
+  ['MusicBrainz Release Track Id', 'releaseTrackID'],
+  ['MusicBrainz Track Id', 'trackID'],
+  ['MusicIP PUID', 'puid'],
+  ['Acoustid Id', 'acoustID'],
+  ['ARTISTS', 'artists'],
+  ['CATALOGNUMBER', 'catalogID'],
+  ['DJMIXER', 'mixedBy'],
+  ['LABEL', 'label'],
+  ['LANGUAGE', 'language'],
+  ['MEDIA', 'media'],
+  ['SCRIPT', 'script'],
+  ['ORIGINALYEAR', 'originalYear'],
+  ['ORIGINALDATE', 'originalDate']
+])
