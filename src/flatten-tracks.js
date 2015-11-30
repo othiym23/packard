f7855dfc1f13b906f4a7da43183c79bfd527acc9
Forrest L Norvell <forrest@npmjs.com>
2015-11-30 01:10:28 -0800

    flatten: hack around issue with single-track albums
    Need to add cuesheet parsing

--- src/flatten-tracks.js
+++ src/flatten-tracks.js
@@ -2,7 +2,7 @@ export default function flatten (artists) {
   const tracks = new Set()
   for (let fsArtist of artists)
     for (let fsAlbum of fsArtist.albums)
-      for (let fsTrack of fsAlbum.tracks)
+      for (let fsTrack of (fsAlbum.tracks || []))
         tracks.add({ fsArtist, fsAlbum, fsTrack, path: fsTrack.file.path })
 
   return tracks
