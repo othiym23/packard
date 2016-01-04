430c46ca04ecc1c71e172c4a4938da0bb0b85680
Forrest L Norvell <forrest@npmjs.com>
2016-01-03 16:57:23 -0800

    optimize: use extracted knapsack, just like pack

--- src/command/optimize.js
+++ src/command/optimize.js
@@ -1,6 +1,7 @@
-import { resolve as optimize } from 'knapsack-js'
 import log from 'npmlog'
 
+import optimize from '../utils/knapsack-albums.js'
+import { bySize } from '../utils/sort.js'
 import { scanAlbums } from './albums.js'
 
 export default function optimizeAlbums (
@@ -20,30 +21,11 @@ export default function optimizeAlbums (
 
 function calculate (albums, blockSize, capacity) {
   log.verbose('calculate', 'blockSize', blockSize, 'capacity', capacity)
-  // used for displaying report
-  const al = albums.reduce((c, a) => c.set(a.path, a), new Map())
-
-  // used for difference calculation later
-  const paths = new Set(al.keys())
-
-  // knapsack-js wants a list of object literals, which is hard to get directly
-  // from a map
-  const sizes = albums.reduce(
-    (c, a) => { c.push({ [a.path]: a.getSize(blockSize) }); return c },
-    []
-  )
-  log.verbose('calculate', 'sizes', sizes)
-  const optimized = optimize(capacity, sizes)
-  log.verbose('calculate', 'optimized', optimized)
-  const keepers = new Set(optimized.map(i => Object.keys(i)[0]))
-  log.silly('calculate', 'keepers', keepers)
-  const leftovers = [...paths].filter(p => !keepers.has(p))
-  log.silly('calculate', 'leftovers', leftovers)
+  const { included, discarded } = optimize(albums, capacity, blockSize)
 
   console.log('included:')
   let includedSize = 0
-  for (let path of [...keepers].sort((a, b) => al.get(b).getSize(1) - al.get(a).getSize(1))) {
-    const album = al.get(path)
+  for (let album of included.sort(bySize)) {
     console.log('%s [%s blocks]', album.path, album.getSize(blockSize))
     includedSize += album.getSize(blockSize)
   }
@@ -54,8 +36,7 @@ function calculate (albums, blockSize, capacity) {
 
   console.log('left out:')
   let leftoverSize = 0
-  for (let path of leftovers.sort((a, b) => al.get(b).getSize(1) - al.get(a).getSize(1))) {
-    const album = al.get(path)
+  for (let album of discarded.sort(bySize)) {
     console.log('%s [%s blocks]', album.path, album.getSize(blockSize))
     leftoverSize += album.getSize(blockSize)
   }
