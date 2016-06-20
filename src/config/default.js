ec6b1ea8e1e4702d170238e9e697874740c0ccfb
Forrest L Norvell <forrest@npmjs.com>
2016-06-19 20:00:09 -0700

    config: use npmlog@3's built-in theming

--- src/config/default.js
+++ src/config/default.js
@@ -1,4 +1,3 @@
-import log from 'npmlog'
 import rc from 'rc'
 
 const config = rc(
@@ -16,22 +15,6 @@ const config = rc(
   },
   [] // don't want rc interpreting argv
 )
-
-log.setGaugeTemplate([
-  {type: 'name', separated: true, maxLength: 40, minLength: 40, align: 'left'},
-  {type: 'spinner', separated: true},
-  {type: 'startgroup'},
-  {type: 'completionbar'},
-  {type: 'endgroup'}
-])
-log.gauge.setTheme({
-  startgroup: '╢',
-  endgroup: '╟',
-  complete: '█',
-  incomplete: '░',
-  spinner: '◴◷◶◵',
-  subsection: '→'
-})
 
 // Babel object export doesn't do hoisting, so leave this here
 export default config
